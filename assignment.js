74d7e990e145e4ca61dd192e0681721095b10f48
seonyeong719 <sy904280@gmail.com>
2022-12-02 23:29:46 +0900

    content 추가
    ive, (여자)아이들 추가

--- assignment.js
+++ assignment.js
@@ -1,48 +1,91 @@
 const musics = [
-    {
-        music_title: '사건의 지평선',
-        lyricist: '윤하',
-        song_writer: '윤하, JEWNO(손준호)',
-        arranger: 'JEWNO(손준호), 숀(SHAUN)',
-        singer: '윤하',
-        lyrics: '사건의 지평선 너머로...',
-        comment: {
-            id: 1,
-            userNick: '오르트구름들어라',
-            content: '너무 좋다',
-            creatAt: 2022 - 12 - 01,
-            comment_reply: [
-                {
-                    id: 234,
-                    userNick: '우리가헤어진진짜이유들어라',
-                    content: '뮤직비디오도 봐라',
-                    reply_creatAt: 2022 - 12 - 02,
-                },
-                // {...},
-            ],
+  {
+    music_title: "사건의 지평선",
+    lyricist: "윤하",
+    song_writer: "윤하, JEWNO(손준호)",
+    arranger: "JEWNO(손준호), 숀(SHAUN)",
+    singer: "윤하",
+    lyrics: "사건의 지평선 너머로...",
+    comment: {
+      id: 1,
+      userNick: "오르트구름들어라",
+      content: "너무 좋다",
+      creatAt: 2022 - 12 - 01,
+      comment_reply: [
+        {
+          id: 234,
+          userNick: "우리가헤어진진짜이유들어라",
+          content: "뮤직비디오도 봐라",
+          reply_creatAt: 2022 - 12 - 02,
         },
+      ],
     },
-    {
-        music_title: 'Pink Venom',
-        lyricist: 'BLACKPINK',
-        song_writer: 'Danny Chung, TEDDY, Danny Chung',
-        arranger: 'TEDDY, 24, R.Tee, IDO, TEDDY, 24, R.Tee, IDO',
-        singer: 'BLACKPINK',
-        lyrics: 'This That Pink Venom...',
-        comment: {
-            id: iloverose,
-            userNick: '로제사랑해',
-            content: '음색이 보물',
-            creatAt: 2022 - 10 - 25,
-            comment_reply: [
-                {
-                    id: ilovejenny,
-                    userNick: '제니가짱임',
-                    content: '로제보다 제니임 하 이 알못...',
-                    reply_creatAt: 2022 - 10 - 26,
-                },
-                // {...},
-            ],
+  },
+  {
+    music_title: "Pink Venom",
+    lyricist: "BLACKPINK",
+    song_writer: "Danny Chung, TEDDY, Danny Chung",
+    arranger: "TEDDY, 24, R.Tee, IDO, TEDDY, 24, R.Tee, IDO",
+    singer: "BLACKPINK",
+    lyrics: "This That Pink Venom...",
+    comment: {
+      id: iloverose,
+      userNick: "로제사랑해",
+      content: "음색이 보물",
+      creatAt: 2022 - 10 - 25,
+      comment_reply: [
+        {
+          id: ilovejenny,
+          userNick: "제니가짱임",
+          content: "로제보다 제니임 하 이 알못...",
+          reply_creatAt: 2022 - 10 - 26,
         },
+      ],
     },
+  },
+  {
+    music_title: "After Like",
+    lyricist: "서지음",
+    song_writer: "Ryan S.Jhun, Anders Nilsen, Andrè Jensen, Iselin Solheim, F. Perren, D. Fekaris",
+    arranger: "Ryan S.Jhun, Anders Nilsen, AVIN(아빈), SLAY(슬레이)",
+    singer: "IVE(아이브)",
+    lyrics: "또 모르지 내 마음이 저 날씨처럼 바뀔지 날 나조차 다 알 수 없으니...",
+    comment: {
+      id: 00,
+      userNick: "신미",
+      content: "시그널 음악을 돋보이게 한거 같습니다",
+      creatAt: 2022 - 12 - 01,
+      comment_reply: [
+        {
+          id: 000,
+          userNick: "이세희",
+          content: "저도 친구들과 어울릴 때 이 노래 많이 불러봤어요",
+          reply_creatAt: 2022 - 11 - 26,
+        },
+      ],
+    },
+  },
+  {
+    music_title: "Nxde",
+    lyricist: "전소연",
+    song_writer: "전소연, Poptime, Kako",
+    arranger: "Poptime, Kako, 전소연",
+    singer: "(여자)아이들",
+    lyrics: "Why you think that bout nude?...",
+    comment: {
+      id: 001,
+      userNick: "이세희",
+      content: "목소리도 쿨! 하게 넘치시네요~하트 ❤️하트~",
+      creatAt: 2022 - 11 - 26,
+      comment_reply: [
+        {
+          id: 002,
+          userNick: "Hyun",
+          content:
+            "여자 지코. 환난이나 위기 때 움츠려들기보다 격파해나가는 방식과 대중의 니즈를 기가 막히게 파악함.",
+          reply_creatAt: 2022 - 10 - 28,
+        },
+      ],
+    },
+  },
 ];
